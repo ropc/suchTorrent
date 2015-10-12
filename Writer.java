6750a954c748f9d42434044ad49696bd7f67d000
Qued79 <jdj62@scarletmail.rutgers.edu>
2015-10-12 03:17:41 +0000

    edit

--- Writer.java
+++ Writer.java
@@ -4,15 +4,20 @@ import java.nio.channels.FileChannel;
 import java.io.File;
 import static java.nio.file.StandardOpenOption.*;
 public class Writer{
+	private int pSize;
     FileChannel fileWriter;
-    public Writer(String Filename){
+    public Writer(String Filename, int pieceSize){
+	pSize = pieceSize;
 	Path file = Paths.get("");//Get the current directory's position.
 	file = FileSystems.getDefault().getPath(file.toAbsolutePath().toString(),Filename);//Get the absolute path of the present directory, and append the desired filename
 	try{	
 	File x = new File(file.toAbsolutePath().toString());
 	x.createNewFile();//create the new file with the specified name in the present directory [You can also say /fakefolder/filename to put it in a subdirectory of the present folder]
 	fileWriter = FileChannel.open(file,READ,WRITE);//Create a file writer for this new file
-	}catch(Exception e){System.err.println("ERROR OPENING FILE:");e.printStackTrace();}
+	}catch(Exception e){
+		System.err.println("Error Opening File. Please try a different filename. Valid Input : program [torrent file] [output file]");
+		//e.printStackTrace();
+		}
     }
    
     public void WriteData(int position, Byte data){//Write one byte at the specified location in the file. Will expand the file if necessary
@@ -42,7 +47,18 @@ public class Writer{
 	fileWriter.write(s,pos);
 	}catch(Exception e){System.err.println("ERROR WRITING TO FILE");}
     }
-   
+	
+    //Write a given message to the file. Has already been checked for validity. Only usable when we get a message=piecesize
+    public void WriteMessage(byte[] array){
+	int msgLength = ByteBuffer.wrap(array).getInt();
+	byte[] block = new byte[msgLength-9];
+	System.arraycopy(array, 13, block, 0, msgLength-9);
+	int pieceIndex = ByteBuffer.wrap(array,5,4).getInt();
+	int beginIndex = ByteBuffer.wrap(array,9,4).getInt();
+	int position = ((pSize*pieceIndex) + beginIndex);
+	WriteData(position,block);
+    }
+
    
 
 
