239a33f2e613ef0c2fd6ea179ad33dd1689b32d8
ropc <itslikeroar2412@gmail.com>
2015-10-28 14:17:33 -0400

    forgot to add these here

---
+++ PeerEvent.java
@@ -0,0 +1,31 @@
+public class PeerEvent<T extends EventPayload> {
+	public final Type type;
+	public final Peer sender;
+	public T payload;
+
+	public PeerEvent(Type type, T payload) {
+		this(type, null, payload);
+	}
+
+	public PeerEvent(Type type, Peer sender) {
+		this(type, sender, null);
+	}
+
+	public PeerEvent(Type type, Peer sender, T payload) {
+		this.type = type;
+		this.sender = sender;
+		this.payload = payload;
+	}
+
+	// public T getPayload() {
+	// 	return payload;
+	// }
+
+	public enum Type {
+		CONNECTION_FAILED,
+		HANDSHAKE_SUCCESSFUL,
+		HANDSHAKE_FAILED,
+	 	MESSAGE_RECEIVED,
+		MESSAGE_TO_SEND
+	}
+}
